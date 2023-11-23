5d30ada301aeb04458e85237610cf772b6628ade
Furkan <furkan5590@hotmail.com>
2023-11-23 18:27:53 +0300

    content-view was changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import "./App.css";
+import { IoPersonOutline } from "react-icons/io5";
+import { SlBasket } from "react-icons/sl";
+import { BiMenuAltRight } from "react-icons/bi";
+import flowerPhoto from "./images/flowerPhoto.png"
+import { CiSearch } from "react-icons/ci";
 
+//import flowerSVG from "./images/flower-photo.svg.svg"
+/* import ReactSVG from 'react-svg';
+import { ReactComponent as flowerSVG } from './images/flowerPhotoSVG.svg'; */
 function App() {
   return (
     <>
       <nav>
         <div className="left-bar">
-          <div className="logo">Logo</div>
+          <div className="logo">F-COMMERCE</div>
         </div>
 
         <div className="right-bar">
           <div className="navbar-articles">
             <ul>
               <li>
-                <a href="">Home</a>
+                <a id="" href="">Home</a>
               </li>
               <li>
                 <a href="">Products</a>
@@ -23,10 +31,15 @@ function App() {
             </ul>
           </div>
           <div className="navbar-icons">
-            <img src="" alt="asd" />
-            <img src="" alt="asd" />
-            <img src="" alt="ads" />
-            <img src="" alt="asd" />
+            <ul>
+              <li><a href=""><SlBasket /></a></li>
+              <li><a href=""><IoPersonOutline /> </a></li>
+              <li> <div className="separater-line"></div></li>
+              <li>  <a href=""><BiMenuAltRight /></a></li>
+            </ul>
+            
+            
+          
           </div>
         </div>
       </nav>
@@ -37,9 +50,16 @@ function App() {
           <div className="search-area-num-content">            
           </div>
           <div className="searchbar">
-            <div className="search-button"></div>
+            <input type="text" placeholder="What are you looking for?" />
+            <div className="search-button"><CiSearch className="searchIcon"/></div>
           </div>
         </div>
+        <div className="search-area-view-image">
+         
+        {/*   <ReactSVG src={flowerSVG} /> */}
+          <img src={flowerPhoto} alt="" />
+        </div>
+
       </div>
 
       <div className="catogories-part">
